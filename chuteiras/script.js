0bbf2a60b04f927085cc21ebcd5197a83d051e42
diegoaraaujo <seu-email@example.com>
2024-01-02 18:18:16 -0300

    retomando antigo javaS, o menu deu erro.

--- chuteiras/script.js
+++ chuteiras/script.js
@@ -1,19 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var nav = document.querySelector('nav');
-    var navOffset = nav.offsetTop;
-    var menuToggle = document.querySelector('.menu-toggle');
-    var menu = document.querySelector('.menu');
-  
-    menuToggle.addEventListener('click', function () {
+  var menuToggle = document.querySelector('.menu-toggle');
+  var menu = document.querySelector('.menu');
+
+  menuToggle.addEventListener('click', function() {
       menu.classList.toggle('show');
-    });
-  
-    window.addEventListener('scroll', function () {
-      if (window.pageYOffset >= navOffset) {
-        nav.classList.add('fixed');
-      } else {
-        nav.classList.remove('fixed');
-        menu.classList.remove('show');
-      }
-    });
-  }); 
+  });
+
+  menuToggle.addEventListener('mouseover', function() {
+      menuToggle.classList.add('hover');
+  });
+
+  menuToggle.addEventListener('mouseout', function() {
+      menuToggle.classList.remove('hover');
+  });
+});

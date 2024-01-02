d18c6e8636f33e3244287860cfb195cefdf8cbad
diegoaraaujo <seu-email@example.com>
2024-01-02 17:14:08 -0300

    rolagem do menu para baixo agora esta inexistente

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var nav = document.querySelector('nav');
+    var navOffset = nav.offsetTop;
     var menuToggle = document.querySelector('.menu-toggle');
     var menu = document.querySelector('.menu');
-
-    menuToggle.addEventListener('click', function() {
-        menu.classList.toggle('show');
+  
+    menuToggle.addEventListener('click', function () {
+      menu.classList.toggle('show');
     });
-
-    menuToggle.addEventListener('mouseover', function() {
-        menuToggle.classList.add('hover');
+  
+    window.addEventListener('scroll', function () {
+      if (window.pageYOffset >= navOffset) {
+        nav.classList.add('fixed');
+      } else {
+        nav.classList.remove('fixed');
+        menu.classList.remove('show');
+      }
     });
-
-    menuToggle.addEventListener('mouseout', function() {
-        menuToggle.classList.remove('hover');
-    });
-});
+  }); 
